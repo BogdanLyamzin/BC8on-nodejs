c2f0f66ebe7a9dee6c28c0c5cc56c29fd3669fce
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-10-07 21:40:08 +0300

    finish lesson 12

---
+++ lesson-12/websocket-basic-example/app.js
@@ -0,0 +1,21 @@
+const ws = new require("ws");
+
+const wsServer = new ws.Server({port: 5000});
+
+const clients = [];
+
+wsServer.on("connection", (newClient)=>{
+    clients.push(newClient);
+    newClient.send("Добро пожаловать к нам!");
+
+    newClient.on("message", (message)=> {
+        console.log(message);
+    })
+
+    clients.forEach(client => {
+        if(client !== newClient){
+            client.send("К нам подключился еще один человек")
+        }
+    })
+    // console.log("Новое подключение фронтенда");
+});
